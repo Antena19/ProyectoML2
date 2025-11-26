cefd0085e64f5d77b4a28d9262d16b46a31f7d5b
Angelina Mendoza Y. <ange.mendoza@duocuc.cl>
2025-11-26 09:14:52 -0300

    kedro viz ok

--- src/proyecto_ml/pipeline_registry.py
+++ src/proyecto_ml/pipeline_registry.py
@@ -6,6 +6,7 @@ from proyecto_ml.pipelines.modelado import pipeline as modelado_pipeline
 from proyecto_ml.pipelines.ciencia_datos import pipeline as ciencia_pipeline
 from proyecto_ml.pipelines.ingenieria_datos import pipeline as ingenieria_pipeline
 from proyecto_ml.pipelines.clasificacion.pipeline import create_pipeline as clasificacion_pipeline
+from proyecto_ml.pipelines.reportes import pipeline as reportes_pipeline
 
 
 def register_pipelines() -> dict[str, Pipeline]:
@@ -14,6 +15,7 @@ def register_pipelines() -> dict[str, Pipeline]:
         "ciencia_datos": ciencia_pipeline.create_pipeline(),
         "ingenieria_datos": ingenieria_pipeline.create_pipeline(),
         "clasificacion": clasificacion_pipeline(),
+        "reportes": reportes_pipeline.create_pipeline(),
         "__default__": modelado_pipeline.create_pipeline(),
     }
 
